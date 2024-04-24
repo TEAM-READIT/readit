a8e47359723f369c9bfd9e1bdc651baa4605a2df
박현춘 <hyunchun24@naver.com>
2024-04-24 17:58:47 +0900

    feat: 오토슬라이드 구현

--- frontend/src/containers/Viewer/ViewerPage.tsx
+++ frontend/src/containers/Viewer/ViewerPage.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export const ViewerPage = () => {
 		const location = useLocation();
 		const article = location.state?.article; 
+		const [isRightOpen, setRightOpen] = useState(true);
+
+		const toggleRight = () => {
+			setRightOpen(!isRightOpen);
+		};
+
 	return (
 		<>
 			<div className="relative flex">
-                <div className="relative flex flex-col w-4/5 h-screen">
+                <div className={`relative flex flex-col w-${isRightOpen ? '4/5' : 'full'} h-screen transition-all duration-500 ease-in-out`}>
 					<div className="relative flex w-full h-2/3">
 						<div className="relative flex flex-col w-1/5 h-full">
 							<div className="w-full h-5/6 bg-yellow-100"></div>
@@ -21,7 +28,7 @@ export const ViewerPage = () => {
 					</div>
 					<div className="w-full h-1/3 bg-orange-500"></div>
 				</div>
-				<div className="w-1/5 h-screen bg-primary-500"></div>
+				<div className={`w-1/5 h-screen bg-primary-500 transition-all duration-500 ease-in-out ${isRightOpen ? 'block' : 'hidden'}`} onClick={toggleRight}></div>
             </div>
 		</>
 	);
