7e3d9013aa1277475ff8ced999016c5fa88db9ff
Youngjoo Oh <johnohwb@gmail.com>
2024-04-29 12:30:31 +0900

    fix: 불필요한 메소드 제거

--- backend/readit/src/main/java/readit/article/domain/repository/ArticleRepository.java
+++ backend/readit/src/main/java/readit/article/domain/repository/ArticleRepository.java
@@ -7,8 +7,7 @@ import readit.article.domain.ArticleType;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article,Integer>, CustomArticleRepository {
-    List<Article> findTop3ByOrderByHitDesc();
-    List<Article> findTop3ByTypeOrderByHitDesc(ArticleType type);
+
 }
 
 interface CustomArticleRepository {
