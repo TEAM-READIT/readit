6592ea3688459f3597982a6096d3edc0f43f0bc8
배정훈 <bjh4395@naver.com>
2024-05-02 02:50:06 +0900

    refactor : FastAPIURI AppConfig 등록

--- backend/readit/src/main/java/readit/common/config/AppConfig.java
+++ backend/readit/src/main/java/readit/common/config/AppConfig.java
@@ -2,6 +2,7 @@ package readit.common.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import readit.article.infra.config.FastAPIURI;
 import readit.auth.infra.google.config.GoogleCredentials;
 import readit.auth.infra.kakao.config.KakaoCredentials;
 import readit.auth.infra.naver.config.NaverCredentials;
@@ -13,7 +14,8 @@ import readit.viewer.config.GPTCredentials;
         KakaoCredentials.class,
         NaverCredentials.class,
         GoogleCredentials.class,
-        GPTCredentials.class
+        GPTCredentials.class,
+        FastAPIURI.class
 })
 public class AppConfig {
 }
