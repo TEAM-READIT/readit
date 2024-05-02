d5ae9307c9e8f984a009bb766991b0b703ba2349
gnaak <o101srb@daum.net>
2024-05-03 00:44:19 +0900

    feat: highlight

--- frontend/src/containers/Viewer/TextArea/MainText.tsx
+++ frontend/src/containers/Viewer/TextArea/MainText.tsx
@@ -24,8 +24,19 @@ export const MainText = ({
 	const location = useLocation();
 	const linkdata = location.state?.linkdata;
 
-	useEffect(() => {
-	}, [highlightedRanges]);
+useEffect(() => {
+	highlightedRanges.forEach((range) => {
+		const selection = window.getSelection();
+		if (selection!.rangeCount > 0) {
+			const range = selection!.getRangeAt(0);
+			// const span = document.createElement('span');
+			// span.style.backgroundColor = `${color}`;
+			// range.surroundContents(span);
+			selection!.removeAllRanges();
+		}
+	});
+}, [highlightedRanges]);
+
 
 	const handleMouseUp = () => {
 		const selection = window.getSelection();
@@ -40,14 +51,28 @@ export const MainText = ({
 			setIsMemoOpen(true);
 		}
 	};
-	console.log(highlightedRanges)
+	console.log('highlightedRanges', highlightedRanges);
 	return (
 		<div className='w-full h-full border-solid border-2 shadow-md bg-white overflow-y-auto'>
 			{article ? (
 				<>
 					<div className='text-2xl font-bold m-[3%]'>{article.title}</div>
 					<div className='text-start mx-[2%] mb-[3%]' onMouseUp={handleMouseUp}>
-						{article.content}
+						{highlightedRanges?.length > 0 ? (
+							<>
+								{highlightedRanges.map((hi, index) => (
+									<div key={index}>
+											{article.content.slice(0, hi.startIndex)}
+										<div className={`bg-${hi.color}-200`}>
+											{article.content.slice(hi.startIndex, hi.endIndex + 1)}
+										</div>
+											{article.content.slice(hi.endIndex + 1, )}
+									</div>
+								))}
+							</>
+						) : (
+							<>{article.content}</>
+						)}
 					</div>
 				</>
 			) : (
