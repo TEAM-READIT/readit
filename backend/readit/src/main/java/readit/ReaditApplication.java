fe4e134d094b6cda0c0af32a9a688f0695a62a5a
배정훈 <bjh4395@naver.com>
2024-05-02 17:55:42 +0900

    feat : Scheduler를 통한 크롤러 api 호출 로직 추가

--- backend/readit/src/main/java/readit/ReaditApplication.java
+++ backend/readit/src/main/java/readit/ReaditApplication.java
@@ -2,10 +2,11 @@ package readit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReaditApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReaditApplication.class, args);
     }
