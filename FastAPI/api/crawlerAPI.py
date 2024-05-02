7da3502e85fe5b482cb386ff4447cf556721cbf3
배정훈 <bjh4395@naver.com>
2024-05-02 10:03:46 +0900

    feat : 뉴스 크롤링 API 비동기 수행 및 BackgroundTasks 처리 로직 추가

--- FastAPI/api/crawlerAPI.py
+++ FastAPI/api/crawlerAPI.py
@@ -1,18 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, BackgroundTasks
 from pydantic import HttpUrl
 from models.news import fetch_news_data
 from schemas.schemas import NewsResponse
-from crawler.articlecrawler import ArticleCrawler
 
 app = FastAPI()
 @app.get("/fetch/link", response_model=NewsResponse)
 def fetch_news(url: HttpUrl):
     return fetch_news_data(url)
 
-@app.post("/start/crawler")
-def start_crawler():
-    crawler = ArticleCrawler()
-    crawler.set_category("정치")
-    crawler.set_date_range('2023-01-02', '2023-01-02')
-    crawler.start()
-    return {"mssage": "Crawler started"}
