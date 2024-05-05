4a960c29715667a12d74b57df44ed2e0469565d6
gnaak <o101srb@daum.net>
2024-05-05 18:39:26 +0900

    feat: memo

--- frontend/src/containers/Viewer/Memo/Memos.tsx
+++ frontend/src/containers/Viewer/Memo/Memos.tsx
@@ -1,14 +1,20 @@
 export const Memos = ({ memos }: { memos: string[] }) => {
 	return (
 		<>
-
-				<div className='text-start flex flex-col gap-10'>
-					{memos.map((memo, index) => (
-						<div key={index}  className='bg-yellow-200 shadow-md max-w-[300px] h-[300px] '>
-							{memo}
-						</div>
-					))}
-				</div>
+			<div className='text-start flex flex-col gap-10'>
+				내가 작성한 메모
+				{memos.length>0 ? (
+					<>
+						{memos.map((memo, index) => (
+							<div key={index} className='bg-yellow-200 shadow-md max-w-[300px] h-[300px] '>
+								{memo}
+							</div>
+						))}
+					</>
+				) : (
+					<>작성한 메모가 없습니다</>
+				)}
+			</div>
 		</>
 	);
 };
