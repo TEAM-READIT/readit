a8b21a54283249187b6e4fdc24254ba833872f0c
배정훈 <bjh4395@naver.com>
2024-05-07 05:07:22 +0900

    !hotfix : 인증 처리를 위해 WebConfig에 JwtArgumentResolver를 추가
    - 컨트롤러 메서드에서 인증 매개변수를 처리하기 위해 JwtArgumentResolver를 추가

--- backend/readit/src/main/java/readit/common/config/WebConfig.java
+++ backend/readit/src/main/java/readit/common/config/WebConfig.java
@@ -3,20 +3,30 @@ package readit.common.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import readit.auth.presentation.JwtArgumentResolver;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final JwtArgumentResolver jwtArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(jwtArgumentResolver);
     }
 }
 
