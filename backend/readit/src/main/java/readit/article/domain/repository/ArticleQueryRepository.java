18f4846b893faddc5a4b7920791ef29d36bf2159
Youngjoo Oh <johnohwb@gmail.com>
2024-05-07 19:31:03 +0900

    refactor: querydsl BooleanExpression Optional 최적화(정렬 기준에 따른 분기문 Optional처리)

--- backend/readit/src/main/java/readit/article/domain/repository/ArticleQueryRepository.java
+++ backend/readit/src/main/java/readit/article/domain/repository/ArticleQueryRepository.java
@@ -58,15 +58,15 @@ public class ArticleQueryRepository {
     }
 
     public BooleanExpression eqArticleCursor(Boolean hit, Integer cursor,Integer hitCursor){
-        if(hit){
-            return Optional.ofNullable(hitCursor)
-                    .map(article.hit::loe)
-                    .orElse(null);
-        } else{
-            return Optional.ofNullable(cursor)
-                    .map(article.id::gt)
-                    .orElse(null);
-        }
+        return Optional.ofNullable(hit)
+                .flatMap(isHit -> {
+                    if (isHit) {
+                        return Optional.ofNullable(hitCursor).map(article.hit::loe);
+                    } else {
+                        return Optional.ofNullable(cursor).map(article.id::gt);
+                    }
+                })
+                .orElse(null);
     }
 
     public BooleanExpression eqArticleHitCursor(Boolean hit, Integer cursor){
@@ -86,15 +86,15 @@ public class ArticleQueryRepository {
     }
 
     public BooleanExpression eqMemberArticleCursor(Boolean hit, Integer cursor,Integer hitCursor){
-        if(hit){
-            return Optional.ofNullable(hitCursor)
-                    .map(memberArticle.article.hit::loe)
-                    .orElse(null);
-        } else{
-            return Optional.ofNullable(cursor)
-                    .map(memberArticle.article.id::gt)
-                    .orElse(null);
-        }
+        return Optional.ofNullable(hit)
+                .flatMap(isHit -> {
+                    if (isHit) {
+                        return Optional.ofNullable(hitCursor).map(memberArticle.article.hit::loe);
+                    } else {
+                        return Optional.ofNullable(cursor).map(memberArticle.article.id::gt);
+                    }
+                })
+                .orElse(null);
     }
 
     public OrderSpecifier<?> sortArticleByHit(Boolean hit){
