7762f3e3d3ea819b3ddd2f25866095755851a533
전하영 <ycis5013@naver.com>
2024-05-09 12:28:34 +0900

    feat: 통계 API 구현 및 문제 불러오기 API 수정

--- backend/readit/src/main/java/readit/article/domain/repository/ArticleQueryRepository.java
+++ backend/readit/src/main/java/readit/article/domain/repository/ArticleQueryRepository.java
@@ -4,12 +4,19 @@ import com.querydsl.core.types.NullExpression;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import readit.article.domain.Article;
+import readit.article.domain.ArticleType;
+import readit.article.domain.QArticle;
 import readit.article.dto.Page;
+import readit.challenge.domain.QMemberProblem;
+import readit.member.domain.Member;
 import readit.viewer.domain.entity.MemberArticle;
 
 import java.util.List;
@@ -23,41 +30,58 @@ import static readit.viewer.domain.entity.QMemberArticle.memberArticle;
 public class ArticleQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public Page<Article> findArticleWithFilter(Integer hitCursor, String category, String title, String content, String reporter, Boolean hit, Integer cursor, Integer limit){
+    public Page<Article> findArticleWithFilter(Integer hitCursor, String category, String title, String content, String reporter, Boolean hit, Integer cursor, Integer limit) {
         List<Article> articles = queryFactory // 모든 글 목록
                 .selectFrom(article)
                 .where(
-                        eqArticleCursor(hit,cursor,hitCursor),
-                        eqArticleHitCursor(hit,cursor),
+                        eqArticleCursor(hit, cursor, hitCursor),
+                        eqArticleHitCursor(hit, cursor),
                         eqArticleCategory(category),
                         eqArticleTitle(title),
                         eqArticleContent(content),
                         eqArticleReporter(reporter)
                 )
                 .orderBy(sortArticleByHit(hit))
-                .limit(limit+1)
+                .limit(limit + 1)
                 .fetch();
-        return new Page<>(articles,limit);
+        return new Page<>(articles, limit);
     }
 
-    public Page<MemberArticle> findMemberArticleWithFilter(Integer hitCursor, String category, String title, String content, String reporter, Boolean hit, Integer cursor, Integer limit){
-        List<MemberArticle> memberArticles =  queryFactory
+    public Page<MemberArticle> findMemberArticleWithFilter(Integer hitCursor, String category, String title, String content, String reporter, Boolean hit, Integer cursor, Integer limit) {
+        List<MemberArticle> memberArticles = queryFactory
                 .selectFrom(memberArticle)
                 .where(
-                        eqMemberArticleCursor(hit,cursor,hitCursor),
-                        eqMemberArticleHitCursor(hit,cursor),
+                        eqMemberArticleCursor(hit, cursor, hitCursor),
+                        eqMemberArticleHitCursor(hit, cursor),
                         eqMemberArticleCategory(category),
                         eqMemberArticleTitle(title),
                         eqMemberArticleContent(content),
                         eqMemberArticleReporter(reporter)
                 )
                 .orderBy(sortMemberArticleByHit(hit))
-                .limit(limit+1)
+                .limit(limit + 1)
                 .fetch();
-        return new Page<>(memberArticles,limit);
+        return new Page<>(memberArticles, limit);
     }
 
-    public BooleanExpression eqArticleCursor(Boolean hit, Integer cursor,Integer hitCursor){
+    public Optional<Article> findNotReadRandomEpigraphy(Member member) {
+        QArticle article = QArticle.article;
+        QMemberProblem memberProblem = QMemberProblem.memberProblem;
+
+        return Optional.ofNullable(queryFactory.selectFrom(article)
+                        .where(article.id.notIn(
+                                JPAExpressions.select(memberProblem.article.id)
+                                        .from(memberProblem)
+                                        .where(memberProblem.member.eq(member))
+                                )
+                                .and(article.type.eq(ArticleType.EPIGRAPHY))
+                        )
+                        .fetchFirst());
+
+    }
+
+
+    public BooleanExpression eqArticleCursor(Boolean hit, Integer cursor, Integer hitCursor) {
         return Optional.ofNullable(hit)
                 .flatMap(isHit -> {
                     if (isHit) {
@@ -69,7 +93,7 @@ public class ArticleQueryRepository {
                 .orElse(null);
     }
 
-    public BooleanExpression eqArticleHitCursor(Boolean hit, Integer cursor){
+    public BooleanExpression eqArticleHitCursor(Boolean hit, Integer cursor) {
         return Optional.ofNullable(hit)
                 .filter(Boolean::booleanValue)
                 .flatMap(h -> Optional.ofNullable(cursor))
@@ -77,7 +101,7 @@ public class ArticleQueryRepository {
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleHitCursor(Boolean hit, Integer cursor){
+    public BooleanExpression eqMemberArticleHitCursor(Boolean hit, Integer cursor) {
         return Optional.ofNullable(hit)
                 .filter(Boolean::booleanValue)
                 .flatMap(h -> Optional.ofNullable(cursor))
@@ -85,7 +109,7 @@ public class ArticleQueryRepository {
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleCursor(Boolean hit, Integer cursor,Integer hitCursor){
+    public BooleanExpression eqMemberArticleCursor(Boolean hit, Integer cursor, Integer hitCursor) {
         return Optional.ofNullable(hit)
                 .flatMap(isHit -> {
                     if (isHit) {
@@ -97,67 +121,65 @@ public class ArticleQueryRepository {
                 .orElse(null);
     }
 
-    public OrderSpecifier<?> sortArticleByHit(Boolean hit){
-        if(BooleanUtils.isTrue(hit)){
-            return new OrderSpecifier<>(Order.DESC,article.hit);
-        }
-        else{
+    public OrderSpecifier<?> sortArticleByHit(Boolean hit) {
+        if (BooleanUtils.isTrue(hit)) {
+            return new OrderSpecifier<>(Order.DESC, article.hit);
+        } else {
             return new OrderSpecifier(Order.ASC, NullExpression.DEFAULT, OrderSpecifier.NullHandling.Default);
         }
     }
 
-    public OrderSpecifier<?> sortMemberArticleByHit(Boolean hit){
-        if(BooleanUtils.isTrue(hit)){
-            return new OrderSpecifier<>(Order.DESC,memberArticle.article.hit);
-        }
-        else{
+    public OrderSpecifier<?> sortMemberArticleByHit(Boolean hit) {
+        if (BooleanUtils.isTrue(hit)) {
+            return new OrderSpecifier<>(Order.DESC, memberArticle.article.hit);
+        } else {
             return new OrderSpecifier(Order.ASC, NullExpression.DEFAULT, OrderSpecifier.NullHandling.Default);
         }
     }
 
-    public BooleanExpression eqArticleContent(String content){
+    public BooleanExpression eqArticleContent(String content) {
         return Optional.ofNullable(content)
                 .map(article.content::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleContent(String content){
+    public BooleanExpression eqMemberArticleContent(String content) {
         return Optional.ofNullable(content)
                 .map(memberArticle.content::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqArticleReporter(String reporter){
+    public BooleanExpression eqArticleReporter(String reporter) {
         return Optional.ofNullable(reporter)
                 .map(article.reporter::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleReporter(String reporter){
+    public BooleanExpression eqMemberArticleReporter(String reporter) {
         return Optional.ofNullable(reporter)
                 .map(memberArticle.article.reporter::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqArticleTitle(String title){
+    public BooleanExpression eqArticleTitle(String title) {
         return Optional.ofNullable(title)
                 .map(article.title::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleTitle(String title){
+    public BooleanExpression eqMemberArticleTitle(String title) {
         return Optional.ofNullable(title)
                 .map(memberArticle.article.title::contains)
                 .orElse(null);
     }
 
-    public BooleanExpression eqArticleCategory(String category){
+    public BooleanExpression eqArticleCategory(String category) {
         return Optional.ofNullable(category)
                 .map(article.category.name::eq)
                 .orElse(null);
     }
 
-    public BooleanExpression eqMemberArticleCategory(String category){
+    public BooleanExpression eqMemberArticleCategory(String category) {
         return Optional.ofNullable(category)
                 .map(memberArticle.article.category.name::eq)
                 .orElse(null);
