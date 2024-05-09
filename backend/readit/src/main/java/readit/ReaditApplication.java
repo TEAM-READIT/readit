d645312de429f47eb5eabae88ca354bee3e526c2
전하영 <ycis5013@naver.com>
2024-05-09 15:51:53 +0900

    chore: 스웨거 서버 url 추가(배포, 개발, 로컬)

--- backend/readit/src/main/java/readit/ReaditApplication.java
+++ backend/readit/src/main/java/readit/ReaditApplication.java
@@ -1,9 +1,18 @@
 package readit;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+        servers={
+                @Server(url="https://readit.store/api/readit", description="Deploy Server URL"),
+                @Server(url="http://k10a705.p.ssafy.io:8080/readit", description="Develop Server URL"),
+                @Server(url="http://localhost:8080/readit", description="Local Server URL")
+        }
+)
 @SpringBootApplication
 //@EnableScheduling
 public class ReaditApplication {
