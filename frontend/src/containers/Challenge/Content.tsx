1e890d79c95fe067fd676ca27540f3858e0b3dfc
gnaak <o101srb@daum.net>
2024-05-14 09:39:43 +0900

    feat: challenge score && line height

--- frontend/src/containers/Challenge/Content.tsx
+++ frontend/src/containers/Challenge/Content.tsx
@@ -12,7 +12,7 @@ const Content = ({ problems }: { problems: ChallengeProps }) => {
 		<>
 			<div className='flex w-1/2 h-4/5'>
 				<Card className='w-full py-10 '>
-					<div className='font-bold text-2xl pb-3 line-loose tracking-wide'>{problems.title}</div>
+					<div className='font-bold text-2xl pb-3 leading-8 tracking-wide'>{problems.title}</div>
 					<div className='text-lg overflow-auto text-start whitespace-pre-wrap px-3'>{realcontent}</div>
 				</Card>
 			</div>
